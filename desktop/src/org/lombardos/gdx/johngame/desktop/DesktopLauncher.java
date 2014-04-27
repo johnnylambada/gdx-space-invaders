5afc95ccfda6bce7526f1ca482214defee17c5ce
John Lombardo <john@lombardos.org>
2014-04-26 21:59:09 -0700

    Initial commit

---
+++ desktop/src/org/lombardos/gdx/johngame/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package org.lombardos.gdx.johngame.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import org.lombardos.gdx.johngame.JohnGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new JohnGame(), config);
+	}
+}
